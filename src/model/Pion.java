56e898a195f9175ddf7513620b6d402481c9b5fb
beetix <b.t.freeman16@gmail.com>
2016-06-01 12:08:42 +0200

    Refactorisation de la classe Pion et ajout du déplacement de 2 cases.

--- src/model/Pion.java
+++ src/model/Pion.java
@@ -17,23 +17,51 @@ public class Pion extends AbstractPiece {
     
     public boolean isMoveOk(int xFinal, int yFinal, boolean isCatchOk, boolean isCastlingPossible)
     {
-        if(Coord.coordonnees_valides(xFinal,yFinal))
+        if (!Coord.coordonnees_valides(xFinal, yFinal))
         {
-            if(isCatchOk)
-            {
-                if( ( (getY()-1==yFinal && couleur==Couleur.NOIR)||(getY()+1==yFinal && couleur==Couleur.BLANC) ) && (getX()+1==xFinal || getX()-1==xFinal) )
-                {
-                    return true;
-                }
-            }
-            else
-            {
-                if( ( (getY()-1==yFinal && couleur==Couleur.NOIR) || ( getY()+1==yFinal && couleur==Couleur.BLANC) ) && getX()==xFinal )
-                {
-                    return true;
-                }
-            }
+            System.err.println("Coordonnées hors du plateau de jeu");
+            return false;
         }
-    return false;
+        
+        if (getX() == xFinal && getY() == yFinal)
+        {
+            System.err.println("Les coordonnées sont les mêmes !");
+            return false;
+        }
+        
+        if(isDeplacementUneCaseNordSud(xFinal, yFinal) || isCatchOk && isDeplacementUneCaseDiagonale(xFinal, yFinal) || isDeplacementDeDeuxAuPremierTour(xFinal, yFinal))
+        {
+            return true;
+        }
+        else
+        {
+           System.err.println("Déplacement impossible, le pion ne peut que"
+                   + " avancer d'une case devant lui ou bien en diagonale"
+                   + " si il peut prendre une autre pièce");
+           return false;
+        }
     }
+
+    private boolean isDeplacementUneCaseNordSud(int xFinal, int yFinal)
+    {
+        return ( (getY() - 1 == yFinal && getCouleur() == Couleur.NOIR) 
+                || ( getY() + 1 == yFinal && getCouleur() == Couleur.BLANC) ) 
+                &&
+                getX() == xFinal;
+    }
+
+    private boolean isDeplacementUneCaseDiagonale(int xFinal, int yFinal)
+    {
+        return ( (getY() - 1 == yFinal && getCouleur() == Couleur.NOIR) 
+                || (getY() + 1 == yFinal && getCouleur() == Couleur.BLANC) ) 
+                &&
+                (getX() + 1 == xFinal || getX() - 1 == xFinal);
+    }
+    
+    private boolean isDeplacementDeDeuxAuPremierTour(int xFinal, int yFinal)
+    {
+        return (  getY() == 1 && yFinal == 3 && getCouleur() == Couleur.BLANC 
+                || getY() == 6 && yFinal == 4 && getCouleur() == Couleur.NOIR );
+    }
+
 }
