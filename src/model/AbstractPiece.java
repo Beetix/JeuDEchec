339b3a4c22c6b7a7b0be3e26f226295db8790905
loupiotte93 <lm.desuremain@gmail.com>
2016-06-01 09:35:47 +0200

    Modifs sur AbstractPiece

--- src/model/AbstractPiece.java
+++ src/model/AbstractPiece.java
@@ -6,22 +6,26 @@
 package model;
 
 /**
- *
- * @author user
+ * Gère le comportement commun de toutes les pièces Chaque classe dérivé (Pion, etc.) sera capable de dire si le déplacement est OK.
+ * @author loupiotte93 & Beetix
  */
 public abstract class AbstractPiece implements Pieces {
     
-    private int x;
-    private int y;
     private Couleur couleur;
     private Coord coord;
     private String name;
     
-    
+    /**
+     * 
+     * @param couleur
+     * @param coord 
+     */
     public AbstractPiece(Couleur couleur, Coord coord)
     {
-        
+        this.coord=coord;
+        this.couleur=couleur;
     }
+    
     
     public int getX(){
         return this.coord.x;
@@ -35,15 +39,27 @@ public abstract class AbstractPiece implements Pieces {
         return this.couleur;
     }
     
-    
+    /**
+     * 
+     * @return le nom de la pièce (Tour, Cavalier, etc.) attention le type ne correspond pas forcément au nom de la classe
+     */
     public java.lang.String getName(){
         return this.name;
     }
     
+    /**
+     * 
+     * @param x
+     * @param y
+     * @return true si déplacement effectué
+     */
     public boolean move(int x,int y){
         return true;
     }
-    
+    /**
+     * 
+     * @return true si piece effectivement capturé Positionne x et y à -1
+     */
     public boolean capture(){
         return true;
     }
@@ -53,6 +69,14 @@ public abstract class AbstractPiece implements Pieces {
         return ""+this.getX()+this.getY();
     }
     
+    /**
+     * 
+     * @param xFinal
+     * @param yFinal
+     * @param isCatchOk
+     * @param isCastlingPossible
+     * @return true si déplacement légal en fonction des algo de déplacement spécifique de chaque pièce
+     */
     public abstract boolean isMoveOk(int xFinal, int yFinal,boolean isCatchOk,boolean isCastlingPossible);
     
     public static void main(){
