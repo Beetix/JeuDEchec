52cc90bfa0b5c1f47b10d332ece9378de1673d66
beetix <b.t.freeman16@gmail.com>
2016-06-01 12:09:23 +0200

    Mise en forme des tests dans le main de AbstractPiece.

--- src/model/AbstractPiece.java
+++ src/model/AbstractPiece.java
@@ -80,24 +80,40 @@ public abstract class AbstractPiece implements Pieces {
     public abstract boolean isMoveOk(int xFinal, int yFinal,boolean isCatchOk,boolean isCastlingPossible);
     
     public static void main(String args[]) {
+        System.setErr(System.out);
+        
         Pieces maTour = new Tour(Couleur.NOIR, new Coord(0, 0));
         
-        maTour.isMoveOk(1, 0, false, false);
-        maTour.isMoveOk(0, 7, false, false);
-        maTour.isMoveOk(7, 0, false, false);
+        System.out.println("Tests censés être OK :");
+        System.out.println("Déplacement de " + maTour + " de 1 vers la droite : " +  (( maTour.isMoveOk(1, 0, false, false) ) ? "OK" : "KO") );
+        System.out.println("Déplacement de " + maTour + " au coin haut gauche : " +  (( maTour.isMoveOk(0, 7, false, false) ) ? "OK" : "KO") );
+        System.out.println("Déplacement de " + maTour + " au coin bas droit : " +  (( maTour.isMoveOk(7, 0, false, false) ) ? "OK" : "KO") );
         
-        maTour.isMoveOk(8, 0, false, false);
-        maTour.isMoveOk(0, 0, false, false);
-        maTour.isMoveOk(1, 1, false, false);
+        System.out.println("Tests censés être KO :");
+        System.out.println("Déplacement de " + maTour + " en dehors du plateau : " +  (( maTour.isMoveOk(8, 0, false, false) ) ? "OK" : "KO") );
+        System.out.println("Déplacement de " + maTour + " au même endroit : " +  (( maTour.isMoveOk(0, 0, false, false) ) ? "OK" : "KO") );
+        System.out.println("Déplacement de " + maTour + " en diagonale : " +  (( maTour.isMoveOk(1, 1, false, false) ) ? "OK" : "KO") );
         
         Pieces monFou = new Fou(Couleur.NOIR, new Coord(2, 0));
         
-        monFou.isMoveOk(3, 1, false, false);
-        monFou.isMoveOk(1, 1, false, false);
-        monFou.isMoveOk(7, 5, false, false);
+        System.out.println("Tests censés être OK :");
+        System.out.println("Déplacement de " + monFou + " d'une case vers le haut et la droite : " +  (( monFou.isMoveOk(3, 1, false, false) ) ? "OK" : "KO") );
+        System.out.println("Déplacement de " + monFou + " d'une case vers le bas et la droite : " +  (( monFou.isMoveOk(1, 1, false, false) ) ? "OK" : "KO") );
+        System.out.println("Déplacement de " + monFou + " vers l'arrête droite : " +  (( monFou.isMoveOk(7, 5, false, false) ) ? "OK" : "KO") );
+
+        System.out.println("Tests censé êtres KO :");
+        System.out.println("Déplacement de " + monFou + " d'une case vers la droite et de 2 vers le haut : " +  (( monFou.isMoveOk(4, 5, false, false) ) ? "OK" : "KO") );
+        System.out.println("Déplacement de " + monFou + " en dehors du plateau : " +  (( monFou.isMoveOk(3, 1, false, false) ) ? "OK" : "KO") );
         
-        monFou.isMoveOk(4, 5, false, false);
-        monFou.isMoveOk(8, 7, false, false);
+        Pieces monPionBlanc = new Pion(Couleur.BLANC, new Coord(0, 1));
+        
+        System.out.println("Tests censés être OK :");
+        System.out.println("Déplacement de " + monPionBlanc + " d'une case vers le haut : " +  (( monPionBlanc.isMoveOk(0, 2, true, false) ) ? "OK" : "KO") );
+        System.out.println("Déplacement de " + monPionBlanc + " d'une case pour prendre un pion présent en haut à droite : " +  (( monPionBlanc.isMoveOk(1, 2, true, false) ) ? "OK" : "KO") );
+        System.out.println("Déplacement de " + monPionBlanc + " de 2 cases vers le haut pour le premier déplacement de ce pion  : " +  (( monPionBlanc.isMoveOk(0, 3, false, false) ) ? "OK" : "KO") );
+
+        
+        
     }
     
 }
